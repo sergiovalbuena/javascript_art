14778e2599b2e849308657f8f5f9f06a8e5c46d7
sergiovalbuena <iosergio.valbuena@gmail.com>
2024-03-06 21:43:22 -0800

    Refactor Point class and add animation loop

--- dots_lines_move/app.js
+++ dots_lines_move/app.js
@@ -1,26 +1,45 @@
-class Point {
-  constructor(x, y, radius = "10", color = "white") {
+class Vector {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.radius = radius;
+  }
+}
+
+class Point {
+  constructor(x, y, color = "white") {
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(randomRange(-1, 1), randomRange(-1, 1));
+    this.radius = randomRange(4, 12);
     this.color = color;
   }
 
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+
   draw(ctx) {
+    ctx.save();
+    ctx.translate(this.pos.x, this.pos.y);
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.arc(0, 0, this.radius, 0, Math.PI * 2);
     ctx.fillStyle = this.color;
     ctx.fill();
+    ctx.strokeStyle = "lightblue";
+    ctx.lineWidth = randomRange(0.1, 2);
+    ctx.stroke();
+    ctx.restore();
   }
+}
+
+function randomRange(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 1080;
 canvas.height = 1080;
-
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 const pointA = new Point(100, 100, 10, "red");
 const pointB = new Point(200, 200, 10, "blue");
@@ -49,6 +68,21 @@ ctx.fill();
 // for (let point of points) {
 //   point.draw(ctx);
 // }
-points.forEach((point) => {
-  point.draw(ctx);
-});
+
+function draw() {
+  // Limpia el canvas
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Actualiza y dibuja tus puntos
+  points.forEach((point) => {
+    point.update();
+    point.draw(ctx);
+  });
+
+  // Solicita el próximo cuadro de la animación
+  window.requestAnimationFrame(draw);
+}
+
+// Inicia la animación
+draw();

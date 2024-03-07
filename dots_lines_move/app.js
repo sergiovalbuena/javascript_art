c5143ae1f31c41b5c070d53eafc0045e7b25c51f
sergiovalbuena <iosergio.valbuena@gmail.com>
2024-03-06 20:58:47 -0800

    Add code for drawing dots on canvas

---
+++ dots_lines_move/app.js
@@ -0,0 +1,35 @@
+class Point {
+  constructor(x, y, radius = "10", color = "white") {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+}
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 1080;
+canvas.height = 1080;
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const pointA = new Point(100, 100, 10, "red");
+const pointB = new Point(200, 200, 10, "blue");
+const pointC = new Point(800, 400);
+
+ctx.beginPath();
+ctx.arc(pointA.x, pointA.y, pointA.radius, 0, Math.PI * 2);
+ctx.fillStyle = pointA.color;
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(pointB.x, pointB.y, pointB.radius, 0, Math.PI * 2);
+ctx.fillStyle = pointB.color;
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(pointC.x, pointC.y, pointC.radius, 0, Math.PI * 2);
+ctx.fillStyle = pointC.color;
+ctx.fill();
